2b464934455b5ac7abcc33755ea6dd19c208944f
Ignacio Guardines <iguardines@garbarino.com.ar>
2017-08-29 16:44:56 -0300

    bootstrap project

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-      //route     cb
-router.get('/', function (req, res) {
-  res.send('Hello Cookie!');
-});
-
-module.exports = router;
